352570f2a86f78994c3f41d6788a021bccdea8eb
DanSousa <dansousac2@gmail.com>
2022-03-11 20:15:01 -0300

    classe escreverNVezes

---
+++ src/atividade01/EscreverNVezes.java
@@ -0,0 +1,18 @@
+package atividade01;
+
+public class EscreverNVezes {
+
+	public static void executar(String msg, int n) {
+		if (n < 1) {
+			return;
+		}
+
+		System.out.println(msg);
+
+		executar(msg, n - 1);
+	}
+
+	public static void main(String[] args) {
+		EscreverNVezes.executar("olá mundo!", 3);
+	}
+}
