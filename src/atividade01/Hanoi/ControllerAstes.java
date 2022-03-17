9a7091b6ac36bfcd94d1a969d26a2b7f52e16207
DanSousa <dansousac2@gmail.com>
2022-03-17 16:43:31 -0300

    retirada classe de TorreDeHanoi

--- src/atividade01/Hanoi/ControllerAstes.java
+++
@@ -1,9 +0,0 @@
-package atividade01.Hanoi;
-
-public class ControllerAstes {
-	
-	public void mudarPecaDeAste(TorreDeHanoiAste inicio, TorreDeHanoiAste fim) {
-		fim.addPeca(inicio.removerPeca());
-	}
-
-}
